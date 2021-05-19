1656bb48bc397a6d072a8ae94bcd5286b8e91840
Phayuth <yphayuth@gmail.com>
2021-05-19 05:58:51 -0400

    ctrl ref to point

--- src/lilnavi/scripts/Ctrl_BSGz.py
+++ src/lilnavi/scripts/Ctrl_BSGz.py
@@ -11,7 +11,7 @@ from tf.transformations import euler_from_quaternion, quaternion_from_euler
 # Reference Pose===================================================================================
 
 def ref_cicle(t):
-	freq   = 2*np.pi/30
+	freq   = 0.01745#2*np.pi/30
 	radius = 3
 
 	x     = radius*np.cos(freq*t)
@@ -19,6 +19,7 @@ def ref_cicle(t):
 
 	xdot  = -radius*freq*np.sin(freq*t)
 	ydot  = radius*freq*np.cos(freq*t)
+	theta_ref = np.arctan2(ydot, xdot)
 
 	xddot = -radius*(freq**2)*np.cos(freq*t)
 	yddot = -radius*(freq**2)*np.sin(freq*t)
@@ -26,29 +27,30 @@ def ref_cicle(t):
 	xdddot= radius*(freq**3)*np.sin(freq*t)
 	ydddot= -radius*(freq**3)*np.cos(freq*t)
 
-	vr    = np.sqrt((xdot**2 + ydot**2))
+	vr    = np.sqrt(((xdot**2) + (ydot**2)))
 	wr    = ((xdot*yddot-ydot*xddot))/((xdot**2 + ydot**2))
 
 	vdotr = (xdot*xddot+ydot*yddot)/vr
 	wdotr = ((xdot*ydddot-ydot*xdddot)/(vr**2))-((2*wr*vdotr)/vr)
 
-	return x,y,vr,wr,ydot,xdot,vdotr,wdotr
+	return x,y,theta_ref,vr,wr,ydot,xdot,vdotr,wdotr
 
-def ref_sin_45(t):
+def ref_8(t):
 	freq   = 0.01745#2*np.pi/30
-	radius = 3
+	a = 3
 
-	x     = (np.cos(np.radians(45))*t)-(np.sin(np.radians(45))*radius*np.sin(freq*t))
-	y     = (np.sin(np.radians(45))*t)+(np.cos(np.radians(45))*radius*np.sin(freq*t))
+	x     = a*np.sin(freq*t)
+	y     = a*np.sin(freq*t)*np.cos(freq*t)
 
-	xdot  = np.cos(np.radians(45))-(np.sin(np.radians(45))*radius*freq*np.cos(freq*t))
-	ydot  = np.sin(np.radians(45))+(np.cos(np.radians(45))*radius*freq*np.cos(freq*t))
+	xdot  = a*np.cos(freq*t)
+	ydot  = a*((np.cos(freq*t))**2 - (np.sin(freq*t))**2)
+	theta_ref = np.arctan2(ydot, xdot)
 
-	xddot = np.sin(np.radians(45))*radius*(freq**2)*np.sin(freq*t)
-	yddot = -np.cos(np.radians(45))*radius*(freq**2)*np.sin(freq*t)
+	xddot = -a*np.sin(freq*t)
+	yddot = -a*4*np.sin(freq*t)*np.cos(freq*t)
 
-	xdddot= np.sin(np.radians(45))*radius*(freq**3)*np.cos(freq*t)
-	ydddot= -np.cos(np.radians(45))*radius*(freq**3)*np.cos(freq*t)
+	xdddot= -a*np.cos(freq*t)
+	ydddot= a*4*((np.cos(freq*t))**2 - (np.sin(freq*t))**2)
 
 	vr    = np.sqrt((xdot**2 + ydot**2))
 	wr    = ((xdot*yddot-ydot*xddot))/((xdot**2 + ydot**2))
@@ -56,7 +58,7 @@ def ref_sin_45(t):
 	vdotr = (xdot*xddot+ydot*yddot)/vr
 	wdotr = ((xdot*ydddot-ydot*xdddot)/(vr**2))-((2*wr*vdotr)/vr)
 
-	return x,y,vr,wr,ydot,xdot,vdotr,wdotr
+	return x,y,theta_ref,vr,wr,ydot,xdot,vdotr,wdotr
 #===================================================================================================
 
 class backstp_contrl:
@@ -91,47 +93,42 @@ class backstp_contrl:
 		""" Control Algorithm for Dynamics, z1= vref-vcur , z2 = wref-wcur, return : tua1c, tua2c """
 		return 1/2*((self.m*self.r*(vdotref+self.ka*z1))+((2*self.r*self.Iner/b)*(wdotref+self.kb*z2))),1/2*((self.m*self.r*(vdotref+self.ka*z1))-((2*self.r*self.Iner/b)*(wdotref+self.kb*z2)))
 
+sq = 0
 def odm_callback(msg):
-
+	global i
 	# Find Current Pose
 	x = msg.pose.pose.position.x
 	y = msg.pose.pose.position.y
 	qq = msg.pose.pose.orientation
 	ol = [qq.x,qq.y,qq.z,qq.w]
 	(rl,pt,yw)=euler_from_quaternion(ol)
-	sq = msg.header.seq
 	qc = np.array([[x],[y],[yw]])
 
 	# Find Desired Pose
-	xRef,yRef,vr,wr,ydot,xdot,vdotref,wdotref = ref_cicle(sq)
-	theta_ref = np.arctan2(ydot, xdot)
+	xRef,yRef,theta_ref,vr,wr,ydot,xdot,vdotref,wdotref = ref_cicle(sq)
 	qr = np.array([[xRef],[yRef],[theta_ref]])
 
 	# Control Input
-	contl = backstp_contrl(10,5,4,100,3000,4,0.1,0.26,2.5) # k1,k2,k3,ka,kb need Tuning
+	contl = backstp_contrl(0.1,4,4,100,3000,4,0.1,0.26,2.5) #run along circle but not on the command one but stable
+	#contl = backstp_contrl(0.00001,0.00001,0.00001,100,3000,4,0.1,0.26,2.5)
 	qe = contl.error(qr,qc)
 	vc,wc = contl.controlkinematic(qe,vr,wr)
 
-	#Limitation of ROBOT
-	if vc > 1:
-		vc = 1
-	if vc < -1:
-		vc = -1
-	if wc > 0.5:
-		wc = 0.5
-	if wc < -0.5:
-		wc = -0.5
+	rospy.loginfo(str(xRef)+" , "+str(yRef)+" , "+str(theta_ref))
+	#rospy.loginfo("delx = "+str(qe[0,0])+" , "+"dely = "+str(qe[1,0])+" , "+"delt ="+str(qe[2,0]))
+	#rospy.loginfo(str(vr)+" , "+str(wr)+" , "+str(theta_ref))
 
 	tw_p = rospy.Publisher("/robotros_test/cmd_vel", Twist, queue_size = 50)
 	Twm = Twist()
 	Twm.linear.x = vc
 	Twm.angular.z = wc
 	tw_p.publish(Twm)
+	sq += 1
 
 def main():
 
 	rospy.init_node('BS_Ctrl')
-	rospy.Subscriber('/robotros_test/odom',Odometry, odm_callback)
+	rospy.Subscriber('/ground_truth/state',Odometry, odm_callback)
 	rospy.spin()
 
 if __name__=='__main__':
